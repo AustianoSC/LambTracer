f1185d292c72d211e8adb53d78c921fcafa5832c
Austin Lamb <arlamb@usc.edu>
2022-07-15 14:59:40 -0700

    Add init files for project.

---
+++ LambTracer/LambTracer.cpp
@@ -0,0 +1,12 @@
+﻿// LambTracer.cpp : Defines the entry point for the application.
+//
+
+#include "LambTracer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
